da4cc72cb433266817f61bbee4db11526e5ab03f
Azuriru <jeannelee823@gmail.com>
2024-05-27 05:49:27 +0800

    Add everything cool like.. search filters and a proper fucking header LOL

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { MouseEventHandler } from 'react';
+import { type MouseEventHandler } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { Icon, Divider } from '$lib/components';
@@ -9,8 +9,8 @@ import { copy } from '$lib/util/array';
 import { capitalize } from '$lib/util/string';
 import { add } from '$lib/redux/cart';
 import { useDispatch } from 'react-redux';
-
 import { useState, useEffect } from 'react';
+import classNames from 'classnames';
 
 import availableProducts from '$lib/util/client/products';
 
@@ -49,7 +49,9 @@ function Product({ name, price, id }: Product) {
 export default function Home() {
     const [products, setProducts] = useState(availableProducts);
     const [filter, setFilter] = useState({});
-    const [sort, setSort] = useState<'alphabetical' | 'price' | 'price-reversed'>('alphabetical');
+    const [filterShown, setFilterShown] = useState(false);
+    const [search, setSearch] = useState('');
+    const [sort, setSort] = useState<'alphabetical' | 'alphabetical-reversed' | 'price' | 'price-reversed'>('alphabetical');
     const categories: string[] = availableProducts.reduce((categories, current) => {
         for (const category of current.category) {
             if (!categories.includes(category)) {
@@ -67,47 +69,91 @@ export default function Home() {
         }));
     };
 
+    const onSearch = (e) => {
+        setSearch(e.target.value);
+    };
+
     useEffect(() => {
-        const sorted = copy(availableProducts).sort((a, b) => {
+        const categories = Object.keys(filter).filter((category) => filter[category]);
+
+        const filtered = copy(availableProducts).filter(({ name, category }) => {
+            return name.includes(search.toLowerCase()) && (!categories.length || category.some((c) => categories.includes(c)));
+        });
+
+        filtered.sort((a, b) => {
             if (sort === 'price') return a.price - b.price;
             if (sort === 'price-reversed') return b.price - a.price;
+            if (sort === 'alphabetical-reversed') return b.name.localeCompare(a.name);
 
             return a.name.localeCompare(b.name);
         });
 
-        const categories = Object.keys(filter).filter((category) => filter[category]);
-
-        if (!categories.length) {
-            return setProducts(sorted);
-        }
-
-        const filteredProducts = sorted.filter(({ category }) => {
-            return category.some((c) => categories.includes(c));
-        });
-
-        setProducts(filteredProducts);
-    }, [sort, filter]);
+        setProducts(filtered);
+    }, [search, sort, filter]);
 
     return (
-        <div className={styles.products}>
-            <div className={styles.sortBy}>
-                <div className={styles.sort} onClick={() => setSort('alphabetical')}>Alphabetical</div>
-                <Divider vr={true} />
-                <div className={styles.sort} onClick={() => setSort('price')}>Price</div>
+        <>
+            <div className={styles.products}>
+                <div className={styles.productHeader}>
+                    <input
+                        name="search"
+                        type="text"
+                        value={search}
+                        className={styles.productSearch}
+                        placeholder="Search for a product"
+                        onInput={onSearch}
+                    />
+                    <div className={styles.productListOptions}>
+                        <div className={styles.productListOption} onClick={() => sort === 'alphabetical' ? setSort('alphabetical-reversed') : setSort('alphabetical')}>
+                            <Icon name="sort_by_alpha" />
+                        </div>
+                        <div className={styles.productListOption} onClick={() => sort === 'price' ? setSort('price-reversed') : setSort('price')}>
+                            <Icon name="swap_vert" />
+                        </div>
+                        <div className={styles.productListOption} onClick={() => setFilterShown(true)}>
+                            <Icon name="tune" />Filters
+                        </div>
+                        <div className={styles.productListOptionFilter}>
+                            {
+                                Object.keys(filter)
+                                    .filter((key) => filter[key])
+                                    .map((category) => {
+                                        return (
+                                            <div key={category} className={styles.productListOption} onClick={() => onFilter(category)}>
+                                                <Icon name="close" /> {capitalize(category)}
+                                            </div>
+                                        );
+                                    })
+                            }
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.productList}>
+                    {
+                        products.map((product) => <Product key={product.id} {...product} />)
+                    }
+                </div>
             </div>
-            <div className={styles.filterBy}>
-                {
-                    categories.map((category) => {
-                        return <div key={category} className={`${styles.category} ${filter[category] ? styles.active : ''}`} onClick={() => onFilter(category)}>{capitalize(category)}</div>;
-                    })
-                }
-                <div className={styles.category} onClick={() => setFilter({})}>Reset All</div>
+            <div className={classNames(styles.productFiltersModal, filterShown && styles.shown)}>
+                <div className={styles.productFiltersHeader}>
+                    Search filters
+                </div>
+                <div className={styles.productFiltersList}>
+                    {
+                        categories.map((category) => {
+                            return (
+                                <div key={category} className={`${styles.category} ${filter[category] ? styles.active : ''}`} onClick={() => onFilter(category)}>
+                                    {capitalize(category)}
+                                </div>
+                            );
+                        })
+                    }
+                </div>
+                <div className={styles.productFiltersControls}>
+                    <div className={styles.productFiltersReset} onClick={() => setFilter({})}>Reset All</div>
+                    <div className={styles.productFilterDone} onClick={() => setFilterShown(false)}>Done</div>
+                </div>
             </div>
-            <div className={styles.productList}>
-                {
-                    products.map((product) => <Product key={product.id} {...product} />)
-                }
-            </div>
-        </div>
+        </>
     );
 }
